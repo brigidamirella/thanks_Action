63e5c77570c2c8169f31aa9479b66ebbc1f0b9e7
brigidamirella <brigidamirella@hotmail.com>
2021-12-22 11:49:31 -0300

    Update action.js

--- src/action.js
+++ src/action.js
@@ -22,9 +22,9 @@ async function run() {
     );
   }
 
-  const randomPos = Math.round(Math.random() * 10);
-  const url = "https://api.tenor.com/v1/search?q=love&pos=$123&limit=1&media_filter=minimal&contentfilter=high&key=HS0UNSXONQCL";
-    
+  const randomPos = Math.round(Math.random() * 1000);
+    const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
+   
   console.log(`Searching Tenor: ${url}`);
 
   const response = await fetch(url);
@@ -45,12 +45,11 @@ async function run() {
 
   const octokit = github.getOctokit(GITHUB_TOKEN);
 
-  await octokit.issues.createComment({
-    // @ts-ignore
+  await octokit.rest.issues.createComment({
     ...context.repo,
     issue_number: pull_request.number,
-    body: `${message}`,
-  });
+    body: `Obrigado por enviar esta pull request. Vamos revisá-la o mais breve possível.\n\n<img src="${gifUrl}" alt="thank you" />`
+});
 }
 
 run().catch((e) => core.setFailed(e.message));
